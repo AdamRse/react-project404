0bc84abdd1a494e631984f752824eb1c97211a3e
AdamRse <bakaarion3@gmail.com>
2024-05-22 14:52:24 +0200

    tuto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import Test from './component/test';
+import Square, { useState } from './component/Square';
 
 function App() {
   return (
-    <Test></Test>
+    <div className='app text-center'>
+      <h1 className='font-bold mb-5 text-3xl'>Stéphane SuperMorpion</h1>
+      <div>
+        <div>
+          <Square number="0" onclick></Square>
+          <Square number="1"></Square>
+          <Square number="2"></Square>
+        </div>
+        <div>
+          <Square number="3"></Square>
+          <Square number="4"></Square>
+          <Square number="5"></Square>
+        </div>
+        <div>
+          <Square number="6"></Square>
+          <Square number="7"></Square>
+          <Square number="8"></Square>
+        </div>
+      </div>
+      <h2 className=''>Généré par superReact et superTailwind</h2>
+    </div>
   );
 }
+
+//Tuto : https://react.dev/learn/tutorial-tic-tac-toe
 
 export default App;
---
+++ src/component/Square.jsx
@@ -0,0 +1,9 @@
+import React, { Component, useState } from 'react'
+
+export default class Square extends Component {
+  render() {
+    return (
+      <button className='w-25 aspect-square bg-slate-100 overflow-hidden border-2 border-slate-800'>stéphane supertest</button>
+    )
+  }
+}
